d5414a0f77490bdc4a00facc76af17ab6be1ebb4
Civoremo <civoremon@gmail.com>
2020-12-02 11:12:57 -0500

    randomize questions order for a particular particular quiz

--- quiz-tourney/src/actions/quizQuestions.js
+++ quiz-tourney/src/actions/quizQuestions.js
@@ -16,7 +16,24 @@ export const getAllQuizQuestions = (quizId) => (dispatch) => {
     url: `${urlApi}/api/quizzes/${quizId}/questions`,
   })
     .then((res) => {
-      dispatch({ type: GET_QUIZ_QUESTIONS_SUCCESS, payload: res.data });
+      let tempPositionsArray = [];
+      while (tempPositionsArray.length < 5) {
+        let selectedPosition = Math.floor(Math.random() * res.data.length);
+        if (!tempPositionsArray.includes(selectedPosition)) {
+          tempPositionsArray.push(selectedPosition);
+        }
+      }
+      let newlyOrderedQuestionsArray = [];
+
+      for (let i = 0; i < 5; i++) {
+        newlyOrderedQuestionsArray[i] = res.data[tempPositionsArray[i]];
+      }
+
+      // dispatch({ type: GET_QUIZ_QUESTIONS_SUCCESS, payload: res.data });
+      dispatch({
+        type: GET_QUIZ_QUESTIONS_SUCCESS,
+        payload: newlyOrderedQuestionsArray,
+      });
     })
     .catch((err) => {
       dispatch({ type: GET_QUIZ_QUESTIONS_FAILURE, payload: err });

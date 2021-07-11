ced34d452f8c7341c8032cd1a4b9de51707ffb86
Civoremo <civoremon@gmail.com>
2021-07-10 22:07:59 -0400

    player avatar canvas added

---
+++ quiz-tourney/src/components/Functions/drawPlayer.js
@@ -0,0 +1,126 @@
+/** @format */
+
+export const drawPlayer = (ctx, numPlayers) => {
+  for (let i = 0; i < numPlayers; i++) {
+    let rightShift = 95 + i * 215;
+
+    ctx.beginPath();
+    ctx.fillStyle = "#1542ab";
+    ctx.moveTo(50 + rightShift, 200);
+    ctx.lineTo(35 + rightShift, 120);
+    ctx.lineTo(140 + rightShift, 120);
+    ctx.lineTo(125 + rightShift, 200);
+    ctx.fill();
+    ctx.closePath();
+
+    //top of podium
+    ctx.beginPath();
+    ctx.fillStyle = "#17629c";
+    ctx.moveTo(25 + rightShift, 120);
+    ctx.lineTo(25 + rightShift, 120);
+    ctx.lineTo(30 + rightShift, 100);
+    ctx.lineTo(145 + rightShift, 100);
+    ctx.lineTo(150 + rightShift, 120);
+    ctx.fill();
+    ctx.closePath();
+
+    //scoreboard
+    ctx.beginPath();
+    ctx.fillStyle = "#b9c1c7";
+    ctx.moveTo(50 + rightShift, 130);
+    ctx.lineTo(125 + rightShift, 130);
+    ctx.lineTo(125 + rightShift, 155);
+    ctx.lineTo(50 + rightShift, 155);
+    ctx.fill();
+    ctx.closePath();
+
+    //torso
+    ctx.beginPath();
+    ctx.fillStyle = "#24a4b5";
+    ctx.moveTo(60 + rightShift, 100);
+    ctx.lineTo(60 + rightShift, 60);
+    ctx.lineTo(110 + rightShift, 60);
+    ctx.lineTo(110 + rightShift, 100);
+    ctx.fill();
+    ctx.closePath();
+
+    //head
+    ctx.beginPath();
+    ctx.fillStyle = "#bdbc9f";
+    ctx.moveTo(85 + rightShift, 60);
+    ctx.arc(85 + rightShift, 40, 20, 0, Math.PI * 2, false);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle = "#222";
+    ctx.moveTo(93 + rightShift, 45);
+    ctx.arc(85 + rightShift, 45, 9, 0, Math.PI, false);
+    ctx.stroke();
+    ctx.closePath();
+
+    //eye left
+    ctx.beginPath();
+    ctx.fillStyle = "#222";
+    ctx.moveTo(80 + rightShift, 40);
+    ctx.arc(78 + rightShift, 35, 3, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+
+    //eye right
+    ctx.beginPath();
+    ctx.fillStyle = "#222";
+    ctx.moveTo(98 + rightShift, 40);
+    ctx.arc(90 + rightShift, 35, 3, 0, Math.PI * 2, true);
+    ctx.fill();
+    ctx.closePath();
+
+    //hair
+    ctx.beginPath();
+    ctx.fillStyle = "#bf2b11";
+    ctx.moveTo(65 + rightShift, 40);
+    ctx.lineTo(55 + rightShift, 38);
+    ctx.lineTo(68 + rightShift, 33);
+    ctx.lineTo(50 + rightShift, 27);
+    ctx.lineTo(70 + rightShift, 25);
+    ctx.lineTo(65 + rightShift, 20);
+    ctx.lineTo(75 + rightShift, 19);
+    ctx.lineTo(75 + rightShift, 12);
+    ctx.lineTo(80 + rightShift, 19);
+    ctx.lineTo(88 + rightShift, 10);
+    ctx.lineTo(92 + rightShift, 19);
+    ctx.lineTo(100 + rightShift, 15);
+    ctx.lineTo(102 + rightShift, 25);
+    ctx.lineTo(110 + rightShift, 30);
+    ctx.lineTo(105 + rightShift, 37);
+    ctx.lineTo(117 + rightShift, 43);
+    ctx.lineTo(104 + rightShift, 53);
+    ctx.lineTo(100 + rightShift, 40);
+    ctx.lineTo(94 + rightShift, 28);
+    ctx.lineTo(88 + rightShift, 26);
+    ctx.lineTo(77 + rightShift, 29);
+    ctx.fill();
+    ctx.closePath();
+
+    //left arm
+    ctx.beginPath();
+    ctx.fillStyle = "#258794";
+    ctx.moveTo(60 + rightShift, 60);
+    ctx.lineTo(50 + rightShift, 62);
+    ctx.lineTo(45 + rightShift, 105);
+    ctx.lineTo(55 + rightShift, 105);
+    ctx.lineTo(63 + rightShift, 60);
+    ctx.fill();
+    ctx.closePath();
+
+    //right arm
+    ctx.beginPath();
+    ctx.fillStyle = "#258794";
+    ctx.moveTo(110 + rightShift, 60);
+    ctx.lineTo(120 + rightShift, 62);
+    ctx.lineTo(125 + rightShift, 105);
+    ctx.lineTo(115 + rightShift, 105);
+    ctx.lineTo(107 + rightShift, 60);
+    ctx.fill();
+    ctx.closePath();
+  }
+};

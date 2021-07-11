51e67c85943b4758781f3c13a1e70eedfe9f9809
Civoremo <civoremon@gmail.com>
2021-07-11 14:15:56 -0400

    player info text canvas

---
+++ quiz-tourney/src/components/canvas/playerTextCanvas.js
@@ -0,0 +1,44 @@
+/** @format */
+
+import React, { useRef, useState, useEffect } from "react";
+
+const PlayerTextCanvas = ({ quizzes }) => {
+  const playerTextCanvasRef = useRef(null);
+  const [playerInfo, setPlayerInfo] = useState([
+    {
+      playerName: "Player 1",
+      playerScore: 0,
+    },
+    {
+      playerName: "Player 2",
+      playerScore: 0,
+    },
+    {
+      playerName: "Player 3",
+      playerScore: 0,
+    },
+  ]);
+
+  useEffect(() => {
+    const cnvs = playerTextCanvasRef.current;
+    const ctx = cnvs.getContext("2d");
+
+    drawPlayerText(ctx, playerInfo);
+  }, [quizzes, playerInfo]);
+
+  return (
+    <canvas
+      id='player-text-canvas'
+      width={800}
+      height={195}
+      ref={playerTextCanvasRef}
+      style={{
+        zIndex: 7,
+        position: "absolute",
+        top: "530px",
+      }}
+    />
+  );
+};
+
+export default playerTextCanvasRef;

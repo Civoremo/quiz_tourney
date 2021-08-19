a212fec5227dac7bc903a0b30185d0199bec5572
Civoremo <civoremon@gmail.com>
2021-08-19 15:48:45 -0400

    home component

---
+++ quiz-tourney/src/components/home.js
@@ -0,0 +1,14 @@
+/** @format */
+
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Home = () => {
+  return (
+    <div>
+      <div>Home component</div>
+    </div>
+  );
+};
+
+export default Home;
